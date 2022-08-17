e5f437233269a36277952f70c9f66d039675259a
mrb34n135421234 <jasonchristian1234@gmail.com>
2022-08-17 13:27:21 +1200

    chore: change react router dom V6 to V5

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,8 @@
 import React from "react";
 import {
   Route,
-  Routes,
   NavLink,
-  BrowserRouter
+  BrowserRouter,
 } from "react-router-dom";
 
 import Home from "./pages/Home";
@@ -14,23 +13,21 @@ import SubmitArticle from "./pages/Submit-Article";
 const App = () =>  {
     return (
       <BrowserRouter>
-      <Routes>
         <Route>
         <div>
           <h1>Software Engineering Empirical Evidence Database (SEED)</h1>
             <ul className="header">
-                <li><NavLink exact='true' to = "/">Home</NavLink></li>
+                <li><NavLink exact to = "/">Home</NavLink></li>
                 <li><NavLink to = "/SEPractice">Select the Practice</NavLink></li>
                 <li><NavLink to = "/SubmitArticle">Submit an Article</NavLink></li>
             </ul>
           <div className="content">
-            <Route exact path="/" element={Home}/>
-            <Route  path="/SEPractice" element={SEPractice}/>
-            <Route  path="/SubmitArticle" element={SubmitArticle}/>
+            <Route exact path="/" component={Home}/>
+            <Route  path="/SEPractice" component={SEPractice}/>
+            <Route  path="/SubmitArticle" component={SubmitArticle}/>
           </div>
         </div>
         </Route>
-        </Routes>
         </BrowserRouter>
     );
 }
